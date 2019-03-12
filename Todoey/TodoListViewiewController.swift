2dcda1dcef893753f6de37132e9d3ed24127b562
David Phillips <David.Phillips.Dev@gmail.com>
2019-03-12 14:56:38 -0500

    Add item complete.

--- Todoey/TodoListViewiewController.swift
+++ Todoey/TodoListViewiewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-	let itemArray = ["Do #1", "Do #2", "Do #3"]
+	var itemArray = ["Do #1", "Do #2", "Do #3"]
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	
 	//MARK - Tableview Datasource Methods
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,8 +30,11 @@ class TodoListViewController: UITableViewController {
 		return cell
 	}
 	
+	
 	//MARK - TableView Delegate Methods
+	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
 		// print ("didSelectRowAt \(indexPath.row): \(itemArray[indexPath.row])")
 		if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
 			tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -40,5 +44,32 @@ class TodoListViewController: UITableViewController {
 		tableView.deselectRow(at: indexPath, animated: true)
 	}
 
+	
+	//MARK - Add New Items
+	
+	@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+		var textField = UITextField()
+		
+		let alert = UIAlertController (title: "Add New Item",
+									   message: "",
+									   preferredStyle: .alert)
+		
+		let action = UIAlertAction (title: "Add Item",
+									style: .default) {
+			(action) in
+			// what will happen once the user clicks the Add Item button
+			self.itemArray.append (textField.text!)
+			self.tableView.reloadData()
+		}
+		
+		alert.addTextField { (alertTextField) in
+			alertTextField.placeholder = "Create new item"
+			textField = alertTextField  // textfield stays in scope long enough to get text field value
+		}
+		
+		alert.addAction(action)
+		present (alert, animated: true, completion: nil)
+	}
+	
 }  // class TodoListViewController
 
